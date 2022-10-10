7283f1efa510c8e3e2a37e2ef229b4d992fe3121
Domino731 <d.orzechowski18@gmail.com>
2022-10-10 20:59:36 +0200

    basics 2

--- JAVA-basics/src/Main.java
+++ JAVA-basics/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +7,30 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         // read name from terminal
         String name = scanner.nextLine();
+
+        // ARRAYS - 2 ways to declare
+        String[] constUsers = new String[4];
+        constUsers[0] = "Norman 1";
+        constUsers[1] = "Norman 2";
+        constUsers[2] = "Norman 3";
+        constUsers[3] = "Norman 4";
+
+        String[] constUsers2 = {"Norman 1", "Norman 2", "Norman 3", "Norman 4"};
+
+        // LIST - something like array above, but u can add more elements
+        List<String> usersList = new ArrayList<String>();
+        usersList.add("Domino");
+
+        // HASHSET - array with unique elements
+        Set<String> meals = new HashSet<String>();
+        meals.add("Pizza");
+        meals.add("Burger");
+        meals.add("Pizza");
+
+        // HASHMAP
+        Map<String, String> nameToMeal = new HashMap<String, String>();
+        nameToMeal.put("Dominik", "Burger");
+        nameToMeal.put("Andrew", "Pizza");
 
         System.out.println("Hello " + name);
     }
